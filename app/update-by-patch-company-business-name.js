c937ba6a8ef133402f7749fb9c33e0acc136da4d
Arisale <arisale@MacBook-Pro-de-Daniel.local>
2023-05-04 16:45:54 -0500

    fix data

--- app/update-by-patch-company-business-name.js
+++ app/update-by-patch-company-business-name.js
@@ -2,10 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateCompanyBusinessName = void 0;
 async function updateCompanyBusinessName(aricoreDb, companyId, businessName) {
-    const result = await aricoreDb.collection('companies').updateOne({ _id: companyId }, { $set: { businessName } });
-    if (result.matchedCount !== 1) {
+    const company = await aricoreDb.collection('companies').findOne({ _id: companyId });
+    if (!company) {
         throw new Error(`ERROR 404 -> Company with ID ${companyId} not found`);
     }
+    if (company.status !== 'ACTIVE') {
+        throw new Error(`ERROR 505 -> Company with ID ${companyId} is not active`);
+    }
+    const result = await aricoreDb.collection('companies').updateOne({ _id: companyId }, { $set: { businessName } });
     if (result.modifiedCount !== 1) {
         throw new Error(`ERROR 500 -> Failed to update company with ID ${companyId}`);
     }
--- src/update-by-patch-company-business-name.ts
+++ src/update-by-patch-company-business-name.ts
@@ -2,13 +2,19 @@ import { Db } from 'mongodb';
 import { CompanyData } from './company-data';
 
 export async function updateCompanyBusinessName(aricoreDb: Db, companyId: string, businessName: string): Promise<void> {
+  const company = await aricoreDb.collection<CompanyData>('companies').findOne({_id: companyId});
+  if (!company) {
+    throw new Error(`ERROR 404 -> Company with ID ${companyId} not found`);
+  }
+  if (company.status !== 'ACTIVE') {
+    throw new Error(`ERROR 505 -> Company with ID ${companyId} is not active`);
+  }
+
   const result = await aricoreDb.collection<CompanyData>('companies').updateOne(
     { _id: companyId },
     { $set: { businessName } },
   );
-  if (result.matchedCount !== 1) {
-    throw new Error(`ERROR 404 -> Company with ID ${companyId} not found`);
-  }
+
   if (result.modifiedCount !== 1) {
     throw new Error(`ERROR 500 -> Failed to update company with ID ${companyId}`);
   }
